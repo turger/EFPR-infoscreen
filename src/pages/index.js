90848d1be6dc637bda2fd4715812a1b077eab8c7
Olli Salo <ullebror@gmail.com>
2024-10-23 13:01:01 +0300

    Added comments to index to show where to add your components

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,20 +20,26 @@ export default function Home() {
                     <div className="flex flex-col justify-between w-2/5 h-full space-y-4">
                         {/* Top Component */}
                         <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2">
-                            <CameraServerComponent />
+                            {/*Runway here */}
                         </div>
 
                         {/* Bottom Component */}
-                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2"></div>
+                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2">
+                            {/* Weather here */}
+                        </div>
                     </div>
 
                     {/*  Middle Container: Two components stacked vertically */}
                     <div className="flex flex-col justify-between w-3/5 h-full space-y-4">
                         {/* Top Component */}
-                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2"></div>
+                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2">
+                            {/*Radar here */}
+                        </div>
 
                         {/* Bottom Component */}
-                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2"></div>
+                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-1/2">
+                            {/* ads-B here */}
+                        </div>
                     </div>
                 </div>
 
@@ -41,11 +47,17 @@ export default function Home() {
                 <div className="w-2/5 h-full flex flex-col justify-between space-y-4">
                     {/* Top: Two components side by side */}
                     <div className="flex space-x-4 h-3/5">
-                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-1/2 h-full"></div>
-                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-1/2 h-full"></div>
+                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-1/2 h-full">
+                            {/* Metar here */}
+                        </div>
+                        <div className="bg-gray-700 rounded-lg shadow-lg p-6 w-1/2 h-full">
+                            <CameraServerComponent />
+                        </div>
                     </div>
                     {/* Bottom: One component */}
-                    <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-full w-full"></div>
+                    <div className="bg-gray-700 rounded-lg shadow-lg p-6 h-full w-full">
+                        {/* Airport Notes here */}
+                    </div>
                 </div>
             </div>
         </div>
