fa34b4f51450f15219059ec4f0d00dba893bb970
Olli Salo <ullebror@gmail.com>
2024-10-29 21:36:05 +0200

    Added image host that needs to be changed when blob is created in vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname:
+                    //change to actual hostname when known
+                    'https://arqjb24dvkuh91jo.public.blob.vercel-storage.com',
+                port: '',
+            },
+        ],
+    },
 };
 
 export default nextConfig;
