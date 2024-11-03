cca96f8fbabe72063780fecdb91ee07adeacc964
Olli Salo <ullebror@gmail.com>
2024-11-03 23:09:55 +0200

    Changed zoom levels to accept fractions and adjusted zoom to be slightly closer

--- src/components/rainRadar/RadarClientComponent.js
+++ src/components/rainRadar/RadarClientComponent.js
@@ -111,7 +111,7 @@ export default function RadarClientComponent({ data }) {
     // Not center on aerodrome, to show rain from west better
     const initialLocation = [61.1, 22.0];
     // Bigger number means closer zoom
-    const initialZoom = 6;
+    const initialZoom = 6.1;
     const [iconSize, setIconSize] = useState(5);
 
     useEffect(() => {
@@ -146,6 +146,7 @@ export default function RadarClientComponent({ data }) {
             center={initialLocation}
             zoom={initialZoom}
             style={{ height: '42vh', width: '100%' }}
+            zoomSnap={0.1}
         >
             <TileLayer
                 url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
