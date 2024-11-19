89235f105d8be879d6cc5e11eb2576546cac01c7
SuviAnnina <suvi.varjoranta@gmail.com>
2024-11-19 17:28:18 +0200

    Add postgres to infonotes

--- src/components/Info/InfoClientComponent.js
+++ src/components/Info/InfoClientComponent.js
@@ -2,25 +2,114 @@
 'use client';
 
 import { useSession, signIn, signOut } from 'next-auth/react';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import LoadingSpinner from '../LoadingSpinner';
+import ErrorComponent from '../ErrorComponent';
 
 export default function InfoClientComponent() {
     const { data: session } = useSession();
-    const [note, setNote] = useState('This is the info screen note');
+    const [note, setNote] = useState('Write a note...');
+    const [allNotes, setAllNotes] = useState([]);
     const [isEditing, setIsEditing] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    // Fetch all notes from database
+    useEffect(() => {
+        fetchNotes();
+    }, []);
+
+    async function fetchNotes() {
+        try {
+            const response = await fetch('/api/infonote');
+            const data = await response.json();
+            //console.log(data);
+            //console.log(data.notes);
+            console.log(data.notes.rows);
+            //console.log(data.rows);
+            if (response.ok) {
+                setAllNotes(data.notes.rows);
+            } else {
+                console.error(data.error);
+            }
+        } catch (error) {
+            console.error('Error fetching notes:', error);
+        }
+    }
 
     const handleNoteChange = (e) => {
         setNote(e.target.value);
     };
 
-    const handleSaveNote = () => {
-        setIsEditing(false); // Hide the textarea when saving
+    // Save the note to the database via the API
+    const handleSaveNote = async () => {
+        try {
+            const response = await fetch('/api/infonote', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ note }),
+            });
+
+            if (!response.ok) {
+                throw new Error('Failed to save note');
+            }
+            setIsEditing(false); // Hide the textarea after saving
+        } catch (err) {
+            setError('Failed to save note. Please try again.');
+        } finally {
+            setIsLoading(false);
+        }
     };
+
+    /*     async function handleSaveNote() {
+            if (!note) return; // Ensure note is not empty
+    
+            setIsLoading(true); // Set loading state
+    
+            try {
+                const response = await fetch('/api/infonote', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ note }) // Send the current note
+                });
+    
+                const data = await response.json();
+    
+                if (response.ok) {
+                    // After saving, re-fetch notes to get the latest state
+                    await fetchNotes();
+                } else {
+                    console.error(data.error);
+                }
+            } catch (error) {
+                console.error('Error saving note:', error);
+            } finally {
+                setIsLoading(false); // Reset loading state after save attempt
+            }
+        } */
+
+    if (error) {
+        return <ErrorComponent message={error?.message} />;
+    }
+    if (isLoading) {
+        return <LoadingSpinner />;
+    }
 
     return (
         <div className="p-4">
             <h1 className="text-2xl font-bold">EHA-Info</h1>
-            <p className="my-4">{note}</p>
+            {/* <p className="my-4">{note || 'No notes'}</p> */}
+            {Array.isArray(allNotes) && allNotes.length > 0 ? (
+                <ul>
+                    {allNotes.map((note, index) => (
+                        <li key={index} className="border-b py-2">
+                            {note.note}
+                        </li>
+                    ))}
+                </ul>
+            ) : (
+                <p>No notes.</p>
+            )}
             {session ? (
                 <div>
                     {isEditing ? (
@@ -34,10 +123,14 @@ export default function InfoClientComponent() {
                             <div className="flex justify-between mt-2">
                                 <button
                                     onClick={handleSaveNote}
+                                    disabled={isLoading}
                                     style={{ backgroundColor: '#fac807' }}
-                                    className="py-1 px-3 hover:bg-blue-500 text-white rounded-md shadow-md text-xs"
+                                    className={`py-1 px-3 text-white rounded-md shadow-md text-xs ${isLoading
+                                        ? 'opacity-50 cursor-not-allowed'
+                                        : 'hover:bg-blue-500'
+                                        }`}
                                 >
-                                    Save
+                                    {isLoading ? 'Saving...' : 'Save'}
                                 </button>
                                 <button
                                     onClick={() => signOut()}
@@ -46,6 +139,9 @@ export default function InfoClientComponent() {
                                     Logout
                                 </button>
                             </div>
+                            {error && (
+                                <p className="text-red-500 mt-2">{error}</p>
+                            )}
                         </div>
                     ) : (
                         <button
